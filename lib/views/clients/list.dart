efcb6648b10f2a20f7ba6b98879fc46fe7426bd0
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-12-23 22:54:24 -0600

    Rename files

--- lib/views/clients/list.dart
+++ lib/views/clients/list.dart
@@ -1,245 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:skyradio_mobile/core/bottom_sheet.dart';
 import 'package:skyradio_mobile/core/dependency_inyection.dart';
+import 'package:skyradio_mobile/core/router.dart';
 import 'package:skyradio_mobile/models/clients.dart';
-import 'package:skyradio_mobile/widgets/avatar.dart';
-import 'package:skyradio_mobile/widgets/badget.dart';
-import 'package:skyradio_mobile/widgets/chart.dart';
-import 'package:skyradio_mobile/widgets/icons.dart';
+import 'package:skyradio_mobile/utils/api_params.dart';
 import 'package:skyradio_mobile/widgets/listview_pagination/sk_listview_pagination.dart';
-import 'package:skyradio_mobile/widgets/tiles/radios.dart';
-import 'package:skyradio_mobile/widgets/search_input.dart';
-import 'package:skyradio_mobile/widgets/tabs.dart';
+import 'package:skyradio_mobile/widgets/scaffold/sk_scaffold.dart';
+import 'package:skyradio_mobile/widgets/tiles/clients.dart';
 
-class ClientView extends StatelessWidget {
-  final Clients client;
-
-  const ClientView({
-    super.key,
-    required this.client,
-  });
+class ClientsView extends StatelessWidget {
+  const ClientsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SkAvatar(
-              color: client.color,
-              alt: client.name,
-            ),
-            const SizedBox(width: 10),
-            Text(
-              client.name,
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              SkBottomSheet.of(context).pushNamed(
-                CLIENTS_ACTIONS_BOTTOM_SHEET,
-                arguments: {'client': client, 'onRefresh': () {}},
-              );
-            },
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 10,
-              right: 10,
-              top: 10,
-            ),
-            child: _Header(client: client),
-          ),
-          const SizedBox(height: 20),
-          _Charts(client: client),
-          const SizedBox(height: 40),
-          Row(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                padding: const EdgeInsets.only(left: 10),
-                child: SkSearchInput(onChanged: (v) {}),
-              ),
-            ],
-          ),
-          Expanded(
-            child: _Radios(client: client),
-          ),
-        ],
-      ),
-      floatingActionButton: Wrap(
-        spacing: 10,
-        direction: Axis.vertical,
-        children: [
-          _ActionsButtons(
-            onPressed: () {},
-            color: const Color.fromRGBO(7, 80, 188, 1),
-            icon: SkIconData.arrows,
-          ),
-          _ActionsButtons(
-            onPressed: () {},
-            color: const Color.fromRGBO(191, 42, 42, 1),
-            icon: SkIconData.arrow_up,
-          ),
-          _ActionsButtons(
-            onPressed: () {},
-            color: const Color.fromRGBO(58, 160, 58, 1),
-            icon: SkIconData.arrow_down,
-          ),
-        ],
-      ),
+    final controller = SkListViewPaginationController(
+      provider: DI.of(context).clientsRepository.getClients,
+      params: ApiParams(filter: ClientsFilter(), sort: ApiSortModel()),
     );
-  }
-}
 
-class _Header extends StatelessWidget {
-  final Clients client;
-
-  const _Header({required this.client});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Wrap(
-        runSpacing: 15,
-        children: [
-          Row(
-            children: [
-              const Text(
-                'Vendedor: ',
-                style: TextStyle(fontSize: 16),
-              ),
-              SkBadge(
-                label: client.seller?.name ?? 'Sin vendedor',
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              const Text(
-                'Modalidad: ',
-                style: TextStyle(fontSize: 16),
-              ),
-              SkBadge(
-                label: client.modality.name,
-                color: client.modality.color,
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class _Radios extends StatelessWidget {
-  final Clients client;
-  const _Radios({required this.client});
-
-  @override
-  Widget build(BuildContext context) {
-    final provider = DI.of(context).clientsRepository.getRadios;
-
-    return SkListViewPagination(
-      controller: SkListViewPaginationController(
-        provider: (params) => provider(client.code, params),
-      ),
-      builder: (radio) => RadiosTile(radio: radio),
-      onTap: (radio) => SkBottomSheet.of(context).pushNamed(
-        RADIO_BOTTOM_SHEET,
-        arguments: radio,
-      ),
-    );
-  }
-}
-
-class _Charts extends StatelessWidget {
-  final Clients client;
-  const _Charts({required this.client});
-
-  @override
-  Widget build(BuildContext context) {
-    final provider = DI.of(context).clientsRepository.getStats;
-
-    return FutureBuilder<ClientsStats>(
-      future: provider(client.code),
-      builder: (_, snapshot) {
-        final models = snapshot.data?.models
-            .map((e) => SkChartData(e.name, e.color, e.count, e.percent))
-            .toList();
-
-        final providers = snapshot.data?.providers
-            .map((e) => SkChartData(e.name, e.color, e.count, e.percent))
-            .toList();
-
-        return SkTabs(
-          tabs: [
-            SkTab(
-              label: 'Modelos',
-              child: SkChart(
-                size: 100,
-                data: models ?? [],
-              ),
-            ),
-            SkTab(
-              label: 'Proveedores',
-              child: SkChart(
-                size: 100,
-                data: providers ?? [],
-              ),
-            ),
-          ],
+    return SkScaffold(
+      title: 'Clientes',
+      controller: controller,
+      builder: (client) => ClientsTile(client: client),
+      onTap: (client) {
+        Navigator.pushNamed(
+          context,
+          CLIENT_VIEW,
+          arguments: client,
         );
       },
-    );
-  }
-}
-
-class _ActionsButtons extends StatelessWidget {
-  final SkIconData icon;
-  final Color color;
-  final void Function() onPressed;
-
-  const _ActionsButtons({
-    required this.icon,
-    required this.color,
-    required this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
-        ),
-        backgroundColor: color,
-        minimumSize: const Size(50, 50),
-      ),
-      child: SkIcon(
-        icon,
-        size: 28,
-        color: Colors.white,
-      ),
+      onListActions: (action) {
+        if (action == SkScaffoldAction.add) {
+          Navigator.pushNamed(context, CLIENT_CREATE_VIEW).then((value) {
+            if (value == true) controller.refresh();
+          });
+        } else if (action == SkScaffoldAction.sort) {
+          SkBottomSheet.of(context).pushNamed(
+            SORT_LIST_BOTTOM_SHEET,
+            arguments: {
+              'sort': controller.params.sort,
+              'onRefresh': controller.refresh,
+            },
+          );
+        } else if (action == SkScaffoldAction.filter) {
+          SkBottomSheet.of(context).pushNamed(
+            CLIENTS_FILTER_BOTTOM_SHEET,
+            arguments: {
+              'filter': controller.params.filter,
+              'onRefresh': controller.refresh,
+            },
+          );
+        }
+      },
+      onItemActions: (client, callback) {
+        SkBottomSheet.of(context).pushNamed(
+          CLIENTS_ACTIONS_BOTTOM_SHEET,
+          arguments: {
+            'client': client,
+            'onRefresh': callback,
+          },
+        );
+      },
     );
   }
 }
